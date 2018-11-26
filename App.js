2933e3a64dd6836d25a95513f5659a0498cbdef8
zhangbooming <BoyangZhang2020@u.northwestern.edu>
2018-11-25 23:26:10 -0600

    add database module to NameTag

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,TouchableHighlight,TextInput  } from 'react-native';
 import { createBottomTabNavigator,createAppContainer } from 'react-navigation';
 import NameTag from './app/screens/NameTag.js';
 import Friends from './app/screens/Friends.js';
@@ -16,6 +16,7 @@ const MainStack = createBottomTabNavigator(
 const MainStack1 = createAppContainer(MainStack);
 
 export default class App extends React.Component {
+
   render() {
     return (
 
--- app/screens/NameTag.js
+++ app/screens/NameTag.js
@@ -1,24 +1,83 @@
 import React from 'react';
-import { FlatList, StyleSheet,Text, View, Image } from 'react-native'
+import { Button, FlatList, StyleSheet, Text, View, Image, TouchableHighlight } from 'react-native'
+import firebase from '../../config/config.js';
 
-class NameTag extends React.Component{
+class NameTag extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
+        this.state={
+            Email:null,
+            Phone:null,
+            UserName:null
+        }
     }
 
-    render()
-    {
-        return(
-            <View style={{flex:1}}>
-            <View style={{height:70,paddingTop:30, backgroundColor:'white', borderColor:'lightgrey',borderBottomWidth:0.5,justifyContent:'center',alignItems:'center'}}>
-                <Text>NameTag</Text>
+    setValue(Email,Phone,UserName){
+        console.log('hhhhhhhhhhhhhh')
+        this.setState({
+            Email:Email,
+            Phone:Phone,
+            UserName:UserName
+        })
+        console.log("success");
+    }
+
+    sendRequest(Code){
+        var that = this;
+        console.log("******************");
+        firebase.database().ref('Codes/'+Code).child('UserId').once('value').then(function(snapshot){
+            const exists = (snapshot.val() != null);
+            if (exists)  {
+              UserId = snapshot.val()
+              console.log(snapshot.val(),"hhhhh")
+              console.log('Single  Value: ',UserId)
+              firebase.database().ref('Users/'+UserId).once('value').then(function(snapshot){
+                const exists = (snapshot.val() != null);
+                if(exists){
+                    Email = snapshot.val().Email
+                    Phone = snapshot.val().Phone
+                    UserName = snapshot.val().UserName
+                    console.log("Email:",Email,"Phone",Phone,"UserName",UserName)
+                    that.setValue(Email,Phone,UserName)
+                }
+              })
+            }
+          }).catch(error => console.log(error));
+    }
+
+
+    render() {
+        return (
+            <View>
+            <View style={{ flex: 1 }}>
+                <View style={{ height: 70, paddingTop: 30, backgroundColor: 'white', borderColor: 'lightgrey', borderBottomWidth: 0.5, justifyContent: 'center', alignItems: 'center' }}>
+                    <Text>NameTag</Text>
+                </View>
             </View>
+            <View style={{ height: 350, paddingTop: 300,  justifyContent: 'center', alignItems: 'center' }} >
+            <Text>HHHHHH</Text>
+
+            <Button
+              onPress = { ()=> this.sendRequest('1234')}
+              title='button' />
+
+            {this.state.Email == null? (<View></View>) :(
+            <View>
+            <Text>Email: {this.state.Email}</Text>
+            <Text>Phone: {this.state.Phone}</Text>
+            <Text>UserName: {this.state.UserName}</Text>
+            </View>
+            )}
+              
+
+            </View>
+
             </View>
         )
     }
 
-
 }
 
 export default NameTag;
+

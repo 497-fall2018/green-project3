821da88199ae8032d169688cd61ba34d831d4195
Sushobhan04 <sushobhanghosh2021@u.northwestern.edu>
2018-11-28 14:07:53 -0600

    text box working

--- app/screens/Search.js
+++ app/screens/Search.js
@@ -15,13 +15,21 @@ class Search extends React.Component{
     onFinishCheckingCode(isValid, code){
       this.setState({
         input: false
-      })  
+      })
     }
 
     closeContact(){
       this.setState({
         input: true
-      })  
+      })
+    }
+    generateRandomNumber(min_value , max_value) {
+      let random_number = Math.random() * (max-min) + min;
+      return Math.floor(random_number);
+    }
+
+    randomElementInArray(array) {
+        return array[generateRandomNumber(0,array.length)];
     }
 
     render()

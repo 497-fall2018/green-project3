14b0b8c82a8172616c96715baaa65817982bd626
Ryan McHenry <ryanmchenry2@gmail.com>
2018-12-05 16:17:53 -0600

    stack navigator implementation

--- App.js
+++ App.js
@@ -1,34 +1,41 @@
 import React from 'react';
 import { StyleSheet, Text, View,TouchableHighlight,TextInput  } from 'react-native';
-import { createBottomTabNavigator,createAppContainer } from 'react-navigation';
+import { createAppContainer, createStackNavigator } from 'react-navigation';
 import Share from './app/screens/Share.js';
 import Search from './app/screens/Search.js';
 import Profile from './app/screens/Profile.js';
 import HomeScreen from './app/screens/HomeScreen.js';
 import Group from './app/screens/Group.js';
 
+const Nav = createStackNavigator({
+  Home:{screen:HomeScreen},
+  Group:{ screen: Group },
+  Search:{ screen: Search },
+  Profile:{ screen: Profile }
+});
 
+export default createAppContainer(Nav);
 
-const MainStack = createBottomTabNavigator(
-  {
-    Home:{ screen: HomeScreen},
-    Group:{ screen: Group },
-    Search:{ screen: Search },
-    Profile:{ screen: Profile }
-  }
-)
-const MainStack1 = createAppContainer(MainStack);
-
-export default class App extends React.Component {
-
-  render() {
-    return (
-
-        <MainStack1/>
-
-    );
-  }
-}
+// const MainStack = createBottomTabNavigator(
+//   {
+//     Home:{ screen: HomeScreen},
+//     Group:{ screen: Group },
+//     Search:{ screen: Search },
+//     Profile:{ screen: Profile }
+//   }
+// )
+// const MainStack1 = createAppContainer(Nav);
+//
+// export default class App extends React.Component {
+//
+//   render() {
+//     return (
+//
+//         <MainStack1/>
+//
+//     );
+//   }
+// }
 
 const styles = StyleSheet.create({
   container: {

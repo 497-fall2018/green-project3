71eb6d29e3bd4809b5a6b19b2426827901aa4eeb
Sushobhan04 <sushobhanghosh2021@u.northwestern.edu>
2018-12-04 18:23:46 -0600

    start and join group buttns

--- app/screens/Group.js
+++ app/screens/Group.js
@@ -125,21 +125,6 @@ const styles = StyleSheet.create({
     borderColor: '#7a42f4',
     borderWidth: 1,
     paddingTop: 100
-   },
-  input: {
-    margin: 15,
-    height: 40,
-    borderColor: '#7a42f4',
-    borderWidth: 1
-   },
-   submitButton: {
-    backgroundColor: '#7a42f4',
-    padding: 10,
-    margin: 15,
-    height: 40,
-   },
-   submitButtonText:{
-      color: 'white'
    }
 })
 
--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { FlatList, StyleSheet,Text, View, Image, Button, Icon, TouchableOpacity, TextInput } from 'react-native';
+import { FlatList, StyleSheet,Text, View, Image, Button, TouchableOpacity, TextInput } from 'react-native';
+import { CheckBox } from 'react-native-elements'
+import Icon from 'react-native-vector-icons/FontAwesome';
 import CodeInput from 'react-native-confirmation-code-input';
 import TimerCountdown from 'react-native-timer-countdown';
 
@@ -8,120 +10,96 @@ class Share extends React.Component{
     constructor(props){
       super(props);
       this.state={
-        input: true,
-        inputCode:"",
-      }
-      this.wordList = [
-           "able","also","ants","area","army","atom","away","baby","back","ball",
-           "band","bank","bare","bark","barn","base","bean","bear","beat","been","bell","belt","bend",
-           "bent","best","bill","bite","blew","blow","blue","boat","body","bone","book","born",
-           "both","bowl","burn","burst","bus","bush","busy","cage","cake","call","calm","came","camp",
-           "card","care","case","cast","cave","cell","cent","city","clay","club","coal","coat",
-           "cold","come","cook","cool","copy","corn","cost","crew","crop","dark","date","dawn","dead","deal",
-           "dear","deep","deer","desk","dirt","dish","does","done","door","down","dozen","draw","drew",
-           "drop","duck","dull","dust","duty","each","earn","east","easy","eat","edge","else","end","even","ever",
-           "face","fact", "fair","fall","farm","fast","fear","feed","feel","feet","fell","felt","few",
-           "fill","film","find","fine","fire","firm","fish","five","flat","flew","flow","food","foot",
-           "form","fort","four","free","from","fuel","full","gain","game","gate","gave","gift","girl","give","glad",
-           "goes","gold","gone","good","gray","grew","grow","gulf","hair","half","hall","hand","hang","hard",
-           "have","heat","held","help","herd","here","hide","high","hill","hold","hole","home","hope","horn",
-           "hour","huge","hung","hunt","hurt","idea","inch","into","iron","jack","join","jump",
-           "just","keep","kept","kids","kill","kind","knew","know","lack","lady","laid","lake","lamp","land","last","late","leaf",
-           "left","life","lift","like","line","lion","lips","list","live","load","long","look","lose","loss","lost","loud",
-           "love","luck","made","mail","main","make","many","mark","mass","meal","mean","meat","meet","mice","mile","milk","mill",
-           "mind","mine","mood","moon","more","most","move","must","name","near","neck","nest","news","next","nice","nine",
-           "none","noon","nose","note","noun","nuts","once","only","onto","open","over","pack","page","paid","pain",
-           "pair","pale","park","part","pass","past","path","pick","pile","pine","pink","pipe","plan","play","plus","poem","poet",
-           "pole","pond","pony","pool","poor","port","post","pour","pull","pure","push","race","rain","rate",
-           "rays","read","real","rear","rest","rice","rich","ride","ring","rise","road","roar","rock",
-           "roll","roof","room","root","rope","rose","rule","rush","safe","said","sail","sale","salt","same","sand","sang","save","seat","seed",
-           "seems","seen","sell","send","sent","sets","ship","shoe","shop","shot","show","shut","sick",
-           "sign","silk","sing","sink","size","skin","slip","slow","snow","soap","soft","soil","sold","some","song","soon","sort","spin","star","stay",
-           "step","stop","such","suit","sure","swam","swim","tail","take","talk","tall","tank","tape","task",
-           "team","tell","tent","term","test","than","that","thee","them","then","they","thin","this",
-           "thou","thus","tide","till","time","tiny","told","tone","took","tool",
-           "torn","town","trap","tree","trip","tube","tune","turn","type","unit","upon","vast","verb","very",
-           "view","vote","wait","walk","wall","want","warm","warn","wash","wave","weak","wear","week",
-           "well","went","were","west","wet","what","when","whom","wide","wife","wild","will","wind","wing","wire","wise","wish","with","wolf",
-           "wood","wool","word","wore","work","yard","year","your","zero","zulu"
-         ]
-      this.code = "0000",
-      this.inputCode = (text) => {
-      this.setState({ inputCode: text })
+        phoneNumberCheck: true,
+        emailCheck: true,
+        linkedinCheck: true,
+        facebookCheck: true,
+        startgroup:false,
+        joinGroup:false,
       }
     }
 
-    generateRandomNumber(min_value , max_value) {
-      let random_number = Math.random() * (max-min) + min;
-      return Math.floor(random_number);
-    }
 
-    randomElementInArray(array) {
-        return array[generateRandomNumber(0,array.length)];
-    }
 
-    onFinishCheckingCode(c){
-      this.setState({
-        input: false,
-        code: c.toString()
-      })
-    }
-
-    closeContact(){
-      this.setState({
-        input: true
-      })
-    }
-
-    verifyCode(c){
-      if (c===this.wordList[0]){
-        alert('code: ' + c + ' worked ')
-      }
-    }
-
-    render()
-    {
-      return(
+    render() {
+      return (
         <View style = {styles.container}>
-          <TextInput style = {styles.input}
-             underlineColorAndroid = "transparent"
-             placeholder = "code"
-             placeholderTextColor = "#9a73ef"
-             autoCapitalize = "none"
-             onChangeText = {this.inputCode}/>
+          <CheckBox style = {styles.checkbox}
+            title='#'
+            checked={this.state.phoneNumberCheck}
+            onPress={() => this.setState({phoneNumberCheck: !this.state.phoneNumberCheck})}
+          />
 
-          <TouchableOpacity
-             style = {styles.submitButton}
-             onPress = {
-                () => this.verifyCode(this.state.inputCode)
-             }>
-             <Text style = {styles.submitButtonText}> Generate </Text>
-          </TouchableOpacity>
-       </View>
-      )
+          <CheckBox style = {styles.checkbox}
+            title='email'
+            checked={this.state.emailCheck}
+            onPress={() => this.setState({emailCheck: !this.state.emailCheck})}
+          />
+
+          <CheckBox style = {styles.checkbox}
+            title='LinkedIn'
+            checked={this.state.linkedinCheck}
+            onPress={() => this.setState({linkedinCheck: !this.state.linkedinCheck})}
+          />
+
+          <CheckBox style = {styles.checkbox}
+            title='Facebook'
+            checked={this.state.facebookCheck}
+            onPress={() => this.setState({facebookCheck: !this.state.facebookCheck})}
+          />
+
+          <Button style = {styles.checkbox}
+            title="Start Group"
+            loading
+            loadingProps={{ size: "large", color: "rgba(111, 202, 186, 1)" }}
+            titleStyle={{ fontWeight: "700" }}
+            buttonStyle={{
+              backgroundColor: "rgba(92, 99,216, 1)",
+              width: 300,
+              height: 45,
+              borderColor: "transparent",
+              borderWidth: 0,
+              borderRadius: 5
+            }}
+            containerStyle={{ marginTop: 20 }}
+            onPress={() => this.setState({startgroup: !this.state.startgroup})}
+          />
+
+          <Button
+            title="Join Group"
+            loading
+            loadingProps={{ size: "large", color: "rgba(111, 202, 186, 1)" }}
+            titleStyle={{ fontWeight: "700" }}
+            buttonStyle={{
+              backgroundColor: "rgba(92, 99,216, 1)",
+              width: 300,
+              height: 45,
+              borderColor: "transparent",
+              borderWidth: 0,
+              borderRadius: 5
+            }}
+            containerStyle={{ marginTop: 20 }}
+            onPress={() => this.setState({joinGroup: !this.state.joinGroup})}
+          />
+        </View>
+
+
+      );
     }
 
 
 }
 
 const styles = StyleSheet.create({
-   container: {
-      paddingTop: 23
-   },
-   input: {
-      margin: 15,
-      height: 40,
-      borderColor: '#7a42f4',
-      borderWidth: 1
-   },
-   submitButton: {
-      backgroundColor: '#7a42f4',
-      padding: 10,
-      margin: 15,
-      height: 40,
-   },
-   submitButtonText:{
-      color: 'white'
+  container: {
+     paddingTop: 23
+  },
+  checkbox: {
+    margin: 100,
+    height: 100,
+    borderColor: '#7a42f4',
+    borderWidth: 1,
+    paddingTop: 100
    }
 })
 

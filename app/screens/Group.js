d0ea2f93116029a317d30583925f69486466c414
zhangbooming <BoyangZhang2020@u.northwestern.edu>
2018-12-06 16:16:15 -0600

    modify the Group.js and add the refresh function to group page

--- app/screens/Group.js
+++ app/screens/Group.js
@@ -5,6 +5,9 @@ import CodeInput from 'react-native-confirmation-code-input';
 import TimerCountdown from 'react-native-timer-countdown';
 import firebase from '../../config/config.js';
 import { Permissions, Contacts } from 'expo';
+import { PullToRefresh } from 'antd-mobile';
+
+
 
 class Group extends React.Component{
 
@@ -15,143 +18,84 @@ class Group extends React.Component{
         code:"ABLE",
         checked: true,
         ready: false,
-        userInfo: [
-          // {
-          //   UserName: 'Amy Farha',
-          //   avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
-          //   subtitle: 'Vice President'
-          // },
-          // {
-          //   UserName: 'Chris Jackson',
-          //   avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
-          //   subtitle: 'Vice Chairman'
-          // },
-        ],
-        userList: [
-          {
-            UserName: 'Amy Farha',
-            avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
-            subtitle: 'Vice President'
-          },
-          {
-            UserName: 'Chris Jackson',
-            avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
-            subtitle: 'Vice Chairman'
-          },
-        ],
+        userInfo: []      
       }
-      this.wordList = [
-           "able","also","ants","area","army","atom","away","baby","back","ball",
-           "band","bank","bare","bark","barn","base","bean","bear","beat","been","bell","belt","bend",
-           "bent","best","bill","bite","blew","blow","blue","boat","body","bone","book","born",
-           "both","bowl","burn","burst","bus","bush","busy","cage","cake","call","calm","came","camp",
-           "card","care","case","cast","cave","cell","cent","city","clay","club","coal","coat",
-           "cold","come","cook","cool","copy","corn","cost","crew","crop","dark","date","dawn","dead","deal",
-           "dear","deep","deer","desk","dirt","dish","does","done","door","down","dozen","draw","drew",
-           "drop","duck","dull","dust","duty","each","earn","east","easy","eat","edge","else","end","even","ever",
-           "face","fact", "fair","fall","farm","fast","fear","feed","feel","feet","fell","felt","few",
-           "fill","film","find","fine","fire","firm","fish","five","flat","flew","flow","food","foot",
-           "form","fort","four","free","from","fuel","full","gain","game","gate","gave","gift","girl","give","glad",
-           "goes","gold","gone","good","gray","grew","grow","gulf","hair","half","hall","hand","hang","hard",
-           "have","heat","held","help","herd","here","hide","high","hill","hold","hole","home","hope","horn",
-           "hour","huge","hung","hunt","hurt","idea","inch","into","iron","jack","join","jump",
-           "just","keep","kept","kids","kill","kind","knew","know","lack","lady","laid","lake","lamp","land","last","late","leaf",
-           "left","life","lift","like","line","lion","lips","list","live","load","long","look","lose","loss","lost","loud",
-           "love","luck","made","mail","main","make","many","mark","mass","meal","mean","meat","meet","mice","mile","milk","mill",
-           "mind","mine","mood","moon","more","most","move","must","name","near","neck","nest","news","next","nice","nine",
-           "none","noon","nose","note","noun","nuts","once","only","onto","open","over","pack","page","paid","pain",
-           "pair","pale","park","part","pass","past","path","pick","pile","pine","pink","pipe","plan","play","plus","poem","poet",
-           "pole","pond","pony","pool","poor","port","post","pour","pull","pure","push","race","rain","rate",
-           "rays","read","real","rear","rest","rice","rich","ride","ring","rise","road","roar","rock",
-           "roll","roof","room","root","rope","rose","rule","rush","safe","said","sail","sale","salt","same","sand","sang","save","seat","seed",
-           "seems","seen","sell","send","sent","sets","ship","shoe","shop","shot","show","shut","sick",
-           "sign","silk","sing","sink","size","skin","slip","slow","snow","soap","soft","soil","sold","some","song","soon","sort","spin","star","stay",
-           "step","stop","such","suit","sure","swam","swim","tail","take","talk","tall","tank","tape","task",
-           "team","tell","tent","term","test","than","that","thee","them","then","they","thin","this",
-           "thou","thus","tide","till","time","tiny","told","tone","took","tool",
-           "torn","town","trap","tree","trip","tube","tune","turn","type","unit","upon","vast","verb","very",
-           "view","vote","wait","walk","wall","want","warm","warn","wash","wave","weak","wear","week",
-           "well","went","were","west","wet","what","when","whom","wide","wife","wild","will","wind","wing","wire","wise","wish","with","wolf",
-           "wood","wool","word","wore","work","yard","year","your","zero","zulu"
-         ]
+           
     }
 
     componentWillMount(){
       this.getGroupInfo();
     }
 
-    generateRandomNumber(min , max) {
-      let random_number = Math.random() * (max-min) + min;
-      return Math.floor(random_number);
-    }
-
-    generateCode() {
-        this.state.code = this.wordList[this.generateRandomNumber(0,this.wordList.length)];
-    }
-
-    onFinishCheckingCode(c){
-      this.setState({
-        input: false,
-        code: c.toString()
-      })
-    }
-
-    closeContact(){
-      this.setState({
-        input: true
-      })
-    }
-
-    verifyCode(c){
-      if (c===this.wordList[0]){
-        alert('code: ' + c + ' worked ')
-      }
-    }
-
-    addUser(user){
-      this.state.userList.push(user)
-    }
-
     keyExtractor = (item, index) => index
 
-    renderItem = ({ item }) => (
-      <ListItem
-        title={item.UserName}
-        subtitle={'Vice President'}
-        leftAvatar={{ source: { uri: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg' } }}
+    renderItem = ({ item, index }) => (
+      <View>
+      <ListItem key={ index }
+        title={ item.UserName }
+        subtitle={ item.Email }
+        leftAvatar={{ source: { uri: item.Image } }}
         onPress={()=>{this.addContactAsync(item)}}
         rightIcon={{ name: 'person-add' }}
       />
+      </View>
     )
 
+
+    refresh(){
+      console.log("refresh has been excuted!!!")
+      this.state.userInfo=[]
+      var that = this;
+      firebase.database().ref('Groups/'+that.state.code).once('value').then(
+        function(snapshot){
+        const exists = (snapshot.val() != null);
+        if (exists)  {
+          that.setState({
+            groupusers:snapshot.val()
+          })
+          console.log("this.state.groupusers:",that.state.groupusers)
+          that.getUsers(that.state.groupusers)
+          setTimeout(that.dummyfunction, 1000);
+        }
+      })
+    }
+
     getGroupInfo(){
-      const param = this.props.navigation.getParam('groupusers', 'failed');
-      this.getUsers(param)
+      //const code_groupusers = this.props.navigation.getParam('groupusers', 'failed');
+      var that = this;
+      const groupcode = this.props.navigation.getParam('groupcode', 'failed');
+      this.setState({code:groupcode});
+      console.log("groupcode:",groupcode);
+      firebase.database().ref('Groups/'+groupcode).on('value',function(snapshot){
+        const exists = (snapshot.val() != null);
+        if (exists)  {
+          that.setState({
+            groupusers:snapshot.val()
+          })
+          console.log("this.state.groupusers:",that.state.groupusers)
+          that.getUsers(that.state.groupusers)
+          setTimeout(that.dummyfunction, 1000);
+        }
+      })
+  
+      
 
-      setTimeout(this.dummyfunction, 1000);
     }
-
-    // userInfoToUserList(userInfo){
-    //   return userInfo.map(info => {
-    //     name: info["UserName"],
-    //     avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
-    //     subtitle: 'Vice President'
-    //   });
-    // }
-
-    dummyfunction(){
-
-    }
+   
+    dummyfunction(){}
 
     getUsers(groupusers){
+      var that = this;
       for(i in groupusers){
         firebase.database().ref('Users/'+groupusers[i]).once('value')
         .then((snapshot) => {
           const exists = (snapshot.val() != null);
           if (exists)  {
-            this.state.userInfo.push(snapshot.val());
-            console.warn("GetUSer:"+snapshot.val()["UserName"]);
-      this.setState({ready: true})
+            that.state.userInfo.push(snapshot.val());
+            //console.log("groupusers Unit:",snapshot.val())
+            //console.log("Phone:",snapshot.val().Phone)
+            console.log("this.state.userInfo:",this.state.userInfo)
+            that.setState({ready: true})
           }
         })
       }
@@ -182,46 +126,35 @@ class Group extends React.Component{
     }
 
     render() {
-      // console.warn("render"+this.props.groupusers)
-      // this.generateCode();
-      // const param = this.props.navigation.getParam('groupcode', 'failed')
-      // var info = this.getGroupInfo(param)
-      // console.warn("GroupRender:", info)
-      // if(this.state.userInfo.length != 0){
-      //   console.warn("GroupRender:", this.state.userInfo[0].UserName)
-      // }
       if (this.state.ready){
         return (
         <View style = {styles.container}>
+           <TouchableOpacity onPress={()=> this.refresh()}>
+            <Text>Reresh</Text>
+            </TouchableOpacity>
           <Badge
             value={this.state.code}
             textStyle={{ color: 'black' }}
-            wrapperStyle = {{backgroundColor: 'black'}}
-            // onPress={() => {this.generateCode()}}
-          />
-        {/* <Text>THIS IS RYAN'S THING: {param}</Text>?*/}
+            wrapperStyle = {{backgroundColor: 'black'}}/>
 
           <FlatList
+            extraData={this.state}
             keyExtractor={this.keyExtractor}
             data={this.state.userInfo}
-            renderItem={this.renderItem}
-          />
+            renderItem={this.renderItem}/>
+
         </View>
+
       );
       }
       else{
         return (
         <View style = {styles.container}>
           <Badge
-            containerStyle={{ backgroundColor: 'purple',
-          borderRadius:0,
-        padding: 0}}
-            value={this.state.code}
+            containerStyle={{ backgroundColor: 'purple', borderRadius:0, padding: 0}}
+            value={ this.state.code }
             textStyle={{ color: 'white', padding:30,
-            fontSize:24, fontWeight:'bold', letterSpacing:2}}
-            // onPress={() => {this.generateCode()}}
-          />
-        {/* <Text>THIS IS RYAN'S THING: {param}</Text>?*/}
+            fontSize:24, fontWeight:'bold', letterSpacing:2}}/>
         </View>
       );
       }
--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -14,19 +14,62 @@ class Home extends React.Component{
       super(props);
 
       this.state={
-        loggedin: true,
+        loggedin: false,
+        userId:'',      
         phoneNumberCheck: true,
         emailCheck: true,
         linkedinCheck: true,
         facebookCheck: true,
         startGroup:false,
         joinGroup:false,
-        userid:'testuser1',
         groupcode:'testcode',
         groupusers:[],
-        db:null
       }
+      this.wordList = [
+        "able","also","ants","area","army","atom","away","baby","back","ball",
+        "band","bank","bare","bark","barn","base","bean","bear","beat","been","bell","belt","bend",
+        "bent","best","bill","bite","blew","blow","blue","boat","body","bone","book","born",
+        "both","bowl","burn","burst","bus","bush","busy","cage","cake","call","calm","came","camp",
+        "card","care","case","cast","cave","cell","cent","city","clay","club","coal","coat",
+        "cold","come","cook","cool","copy","corn","cost","crew","crop","dark","date","dawn","dead","deal",
+        "dear","deep","deer","desk","dirt","dish","does","done","door","down","dozen","draw","drew",
+        "drop","duck","dull","dust","duty","each","earn","east","easy","eat","edge","else","end","even","ever",
+        "face","fact", "fair","fall","farm","fast","fear","feed","feel","feet","fell","felt","few",
+        "fill","film","find","fine","fire","firm","fish","five","flat","flew","flow","food","foot",
+        "form","fort","four","free","from","fuel","full","gain","game","gate","gave","gift","girl","give","glad",
+        "goes","gold","gone","good","gray","grew","grow","gulf","hair","half","hall","hand","hang","hard",
+        "have","heat","held","help","herd","here","hide","high","hill","hold","hole","home","hope","horn",
+        "hour","huge","hung","hunt","hurt","idea","inch","into","iron","jack","join","jump",
+        "just","keep","kept","kids","kill","kind","knew","know","lack","lady","laid","lake","lamp","land","last","late","leaf",
+        "left","life","lift","like","line","lion","lips","list","live","load","long","look","lose","loss","lost","loud",
+        "love","luck","made","mail","main","make","many","mark","mass","meal","mean","meat","meet","mice","mile","milk","mill",
+        "mind","mine","mood","moon","more","most","move","must","name","near","neck","nest","news","next","nice","nine",
+        "none","noon","nose","note","noun","nuts","once","only","onto","open","over","pack","page","paid","pain",
+        "pair","pale","park","part","pass","past","path","pick","pile","pine","pink","pipe","plan","play","plus","poem","poet",
+        "pole","pond","pony","pool","poor","port","post","pour","pull","pure","push","race","rain","rate",
+        "rays","read","real","rear","rest","rice","rich","ride","ring","rise","road","roar","rock",
+        "roll","roof","room","root","rope","rose","rule","rush","safe","said","sail","sale","salt","same","sand","sang","save","seat","seed",
+        "seems","seen","sell","send","sent","sets","ship","shoe","shop","shot","show","shut","sick",
+        "sign","silk","sing","sink","size","skin","slip","slow","snow","soap","soft","soil","sold","some","song","soon","sort","spin","star","stay",
+        "step","stop","such","suit","sure","swam","swim","tail","take","talk","tall","tank","tape","task",
+        "team","tell","tent","term","test","than","that","thee","them","then","they","thin","this",
+        "thou","thus","tide","till","time","tiny","told","tone","took","tool",
+        "torn","town","trap","tree","trip","tube","tune","turn","type","unit","upon","vast","verb","very",
+        "view","vote","wait","walk","wall","want","warm","warn","wash","wave","weak","wear","week",
+        "well","went","were","west","wet","what","when","whom","wide","wife","wild","will","wind","wing","wire","wise","wish","with","wolf",
+        "wood","wool","word","wore","work","yard","year","your","zero","zulu"
+      ]
     }
+
+    generateRandomNumber(min , max) {
+      let random_number = Math.random() * (max-min) + min;
+      return Math.floor(random_number);
+    }
+
+    generateCode() {
+        this.state.groupcode = this.wordList[this.generateRandomNumber(0,this.wordList.length)];
+    }
+
 
     componentDidMount = () =>{
       var that = this;
@@ -86,8 +129,9 @@ class Home extends React.Component{
   }
 
 
-    createGroup(userid){
+    createGroup(userId){
       var that = this;
+      this.generateCode()
       firebase.database().ref('Groups').once('value')
       .then((snapshot) => {
         const exists = (snapshot.val() != null);
@@ -101,7 +145,7 @@ class Home extends React.Component{
           }
         }
       })
-      firebase.database().ref('Groups/' + this.state.groupcode).set([userid]);
+      firebase.database().ref('Groups/' + this.state.groupcode).set([userId]);
       setTimeout(function(){that.navtoGroup()}, 500);
     }
 
@@ -169,8 +213,9 @@ class Home extends React.Component{
       return userobj
     }
 
+    
     navtoGroup(){
-      this.props.navigation.navigate('Group',{groupcode:this.state.groupcode})
+      this.props.navigation.navigate('Group',{ groupcode: this.state.groupcode })
     }
 
     navtoEnterCode(){
@@ -212,7 +257,7 @@ class Home extends React.Component{
             title="Start Group"
             loading
             loadingProps={{ size: "large", color: "rgba(111, 202, 186, 1)" }}
-            onPress={() => this.createGroup(this.state.userid)}
+            onPress={() => this.createGroup(this.state.userId)}
           >
             <Text style={styles.customButtonText}>START GROUP</Text>
           </TouchableOpacity>
